765a8591f2a27a05bf92fd9b2c254f51b589c8b9
Laure <laure.milian@gmail.com>
2016-10-25 14:18:21 +0200

    serveur fonctionnel host 2000

--- appexpress.js
+++ appexpress.js
@@ -2,17 +2,6 @@ var express = require('express');
 
 var app = express();
 
-app.get('/alice.md', function(req, res) {
-	res.sendfile('alice.md');
-});
-
-app.get('/example.md', function(req, res) {
-    res.sendfile('example.md');
-});
-
-app.get( '/menu.json' , function(req, res) {
-	res.sendfile('menu.json');
-});
-
+app.use(express.static('public'));
 
 app.listen(2000);
