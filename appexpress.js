bdf1bf6cbf017a30b2054a5a9bcca8274098450f
Laure <laure.milian@gmail.com>
2016-10-26 12:30:16 +0200

    ajout bodyparse

--- appexpress.js
+++ appexpress.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = express();
 
 app.use(express.static('public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.post('/', function (req, res) {
   res.send('Got a POST request');
+  console.log(req.body.title);
 });
 
+
 app.listen(2000);
+
+
